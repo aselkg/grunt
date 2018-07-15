262c5feca8706b81432f67e9d51b1dbf58fac1af
Eldar AKopiyanov <peacemakerkg@gmail.com>
2018-07-15 15:51:06 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: 'small',
+            width: '30%',
+            quality: 20
+          },{
+            name: 'large',
+            width: '50%',
+            quality: 40
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
